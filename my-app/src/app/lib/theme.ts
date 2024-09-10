dcc13f3d9fa1c3d7dbb0bcfd5d21442f708563b2
Michelly Victoria Nonato Coelho <michelly.nonato@ifood.com.br>
2024-09-10 13:43:39 -0300

    feat: configure project

---
+++ my-app/src/app/lib/theme.ts
@@ -0,0 +1,30 @@
+'use client';
+import { Roboto } from 'next/font/google';
+import { createTheme } from '@mui/material/styles';
+
+const roboto = Roboto({
+  weight: ['300', '400', '500', '700'],
+  subsets: ['latin'],
+  display: 'swap',
+});
+
+const theme = createTheme({
+  typography: {
+    fontFamily: roboto.style.fontFamily,
+  },
+  palette: {
+    mode: 'dark',
+    primary: {
+      main: '#86458e',
+    },
+    secondary: {
+      main: '#6b586b',
+    },
+    background: {
+      default: '#FFF7FA', 
+      paper: '#FFF7FA',
+    }
+  },
+});
+
+export default theme;
